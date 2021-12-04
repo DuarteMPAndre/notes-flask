3da862d978fa0341686b588d47a3f9871e48cc42
DuarteMPAndre <78273429+DuarteMPAndre@users.noreply.github.com>
2021-12-04 16:42:53 +0000

    Add files via upload

---
+++ website/models.py
@@ -0,0 +1,24 @@
+from logging import disable
+from time import timezone
+from typing import ForwardRef
+from flask.helpers import flash, send_file
+from flask.json import JSONDecoder
+from flask_sqlalchemy import FSADeprecationWarning
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
+    notes = db.relationship('Note')
